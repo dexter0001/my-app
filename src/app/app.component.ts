32fca9cae6cc85f91630ff2d5b8cc1a45b786e6e
Ayush Bhatt <ayush.bhatt2@mail.dcu.ie>
2022-11-11 00:17:48 +0000

    Angular- Progress bar implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  root: any;
+  el: any;
+  rootDiv: any;
+
+  constructor () {
+  }
+
+
+  addProgressBar() {
+
+    this.rootDiv = document.getElementById('root-container');
+    // create progress bar element
+    const bar = document.createElement('progress');
+    this.rootDiv.append(bar); 
+    this.fillTheBar(bar);
+
+    // created line break element
+    const linebreak = document.createElement("br");
+    this.rootDiv.append(linebreak);
+
+  };
+
+  fillTheBar(el: any) {
+
+    setTimeout(() => {
+      el.setAttribute('value', '50');
+    }, 1000);
+  
+    // setTimeout(() => {
+    //   el.setAttribute('value', '100');
+    // }, 10000);
+
+  }
+
 }
+
+
