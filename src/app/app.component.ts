37013a80a363ac91f27869c8c5331ce7d00a575e
Ayush Bhatt <ayush.bhatt2@mail.dcu.ie>
2022-11-13 19:48:53 +0000

    Updated using Queue data structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,36 +11,65 @@ export class AppComponent {
   el: any;
   rootDiv: any;
 
+  items: any[] = [];
+  uploadQueue = new Queue(this.items);
+
+
   constructor () {
   }
-
 
   addProgressBar() {
 
     this.rootDiv = document.getElementById('root-container');
-    // create progress bar element
+    
+    // create progress bar
     const bar = document.createElement('progress');
-    this.rootDiv.append(bar); 
-    this.fillTheBar(bar);
-
-    // created line break element
+    bar.style.setProperty('width', '100%');
+    
+    // created line break
     const linebreak = document.createElement("br");
-    this.rootDiv.append(linebreak);
+    
+    // Push it to QUEUE 
+    this.uploadQueue.enque(bar);
+    
+    for (let i = 0; i < this.uploadQueue.getItems().length; i++) {
+      do {
+        let element = this.uploadQueue.getItems();
+        this.rootDiv.append(element[i]);
+        this.fillTheBar(element[i]);
+        this.uploadQueue.dequeue();
+        this.rootDiv.append(linebreak);
+      } while (i != this.uploadQueue.getItems().length);
+    }
 
   };
 
   fillTheBar(el: any) {
 
     setTimeout(() => {
-      el.setAttribute('value', '50');
-    }, 1000);
-  
-    // setTimeout(() => {
-    //   el.setAttribute('value', '100');
-    // }, 10000);
+      el.setAttribute('value', '100');
+    }, 2000);
 
   }
 
 }
 
+class Queue {
+  items: any[];
+  constructor(params: any[]) {
+    this.items = params;
+  }
 
+  enque(item: any) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    // return and move first element of the queue
+    return this.items.shift();
+  }
+
+  getItems() {
+    return this.items;
+  }
+}
